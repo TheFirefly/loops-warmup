a0f2e9d78c4da480ebe00d922ca42e30323d23a4
Sasha Ouellet <spaouellet@me.com>
2013-09-05 08:19:08 -0600

    Finished exponent function

--- LoopsWarmup.java
+++ LoopsWarmup.java
@@ -2,7 +2,8 @@ public class LoopsWarmup {
 
 
 	public static void main(String[] args) {
-		summation(10);
+		//summation(10);
+		exponent(2,10);
 	}
 
 	private static void summation(int n) {
@@ -11,5 +12,13 @@ public class LoopsWarmup {
 			sum += i;
 		}
 		System.out.println("Sum: " + sum);
+	}
+
+	private static void exponent(int x, int n) {
+		int newX = 1;
+		for (int i = 1 ; i <= n ; i++) {
+			newX *= x;
+		}
+		System.out.println(x + "^" + n + ": " + newX);
 	}
 }
